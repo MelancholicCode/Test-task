91b30e1d0b8ef4edc7b763f9741ea39b92e21569
MelCode <madowl052@gmail.com>
2023-03-11 00:06:41 +0300

    complete first task

--- 1 task/index.js
+++ 1 task/index.js
@@ -5,14 +5,38 @@ const timerEl = document.querySelector('span');
 // Напишите реализацию createTimerAnimator
 // который будет анимировать timerEl
 const createTimerAnimator = () => {
-  return (seconds) => {};
+  return (seconds) => {
+    const deadline = Date.parse(new Date()) + seconds * 1000;
+
+    const timer = setInterval(changeTimerStep, 1000);
+    changeTimerStep();
+    buttonEl.disabled = true;
+
+    function getZero(num) {
+      if (num < 10) return  '0' + num;
+      return num;
+    }
+
+    function changeTimerStep() {
+      if (new Date() > deadline) {
+        clearInterval(timer);
+        buttonEl.disabled = false;
+        return;
+      }
+      const timeLeft = deadline - new Date();
+      const hours = getZero(Math.floor(timeLeft / (1000 * 60 * 60) % 24));
+      const minutes = getZero(Math.floor(timeLeft / (1000 * 60) % 60));
+      const seconds = getZero(Math.floor(timeLeft / 1000 % 60));
+
+      timerEl.textContent = `${hours}:${minutes}:${seconds}`;
+    }
+  };
 };
 
 const animateTimer = createTimerAnimator();
 
 inputEl.addEventListener('input', () => {
-  // Очистите input так, чтобы в значении
-  // оставались только числа
+  inputEl.value = inputEl.value.replace(/\D/, '');
 });
 
 buttonEl.addEventListener('click', () => {
